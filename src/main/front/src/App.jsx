7094b5de398884419835c6e1c8001795a503583e
jaeheoon <aleh101@naver.com>
2024-11-15 14:35:26 +0900

    project init

---
+++ src/main/front/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React</h1>
+      <h3>카운트 : 1</h3>
+      <img src="/image/짱구.jpg" alt="짱구" />
+    </div>
+  );
+};
+
+export default App;
